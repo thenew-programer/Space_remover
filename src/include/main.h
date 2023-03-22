4e9d6b0c004dc133e0b032654249764745b384a5
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-03-22 22:52:55 +0000

    Added the file that contains the header files for the functions :

---
+++ src/include/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#define BUFFER_l 128
+#define SP ' '
+void remove_spaces(char *line, char *new_line);
+
+void write_line(char *new_line);
+
+#endif /* HEADER_H */
